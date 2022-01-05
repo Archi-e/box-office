d8599dda02859c7ffbec93333f4f5784299a9833
Archi-e <archie.webwork@gmail.com>
2022-01-05 17:33:50 +0530

    added page layout and title component

--- src/App.js
+++ src/App.js
@@ -6,22 +6,17 @@ import Home from './pages/Home';
 import Starred from './pages/Starred';
 function App() {
   return (
-    <div>
-      <Navs />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/starred">
-          <Starred />
-        </Route>
-        <Route>
-          <div>
-            Not found
-          </div>
-        </Route>
-      </Switch>
-    </div>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
+      <Route>
+        <div>Not found</div>
+      </Route>
+    </Switch>
   );
 }
 
